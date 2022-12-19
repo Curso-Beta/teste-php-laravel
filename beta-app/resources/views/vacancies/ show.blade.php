f926f35f5701c84d7a63bd840d7944513aa71af2
ramonmendes <dwmom@hotmail.com>
2022-12-19 13:57:44 -0300

    Add new html to show vacancies

---
+++ beta-app/resources/views/vacancies/ show.blade.php
@@ -0,0 +1,69 @@
+@extends('vacancies.layout')
+
+  
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-lg-12 margin-tb">
+
+            <div class="pull-left">
+
+                <h2> Show Vacancy</h2>
+
+            </div>
+
+            <div class="pull-right">
+
+                <a class="btn btn-primary" href="{{ route('vacancies.index') }}"> Back</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+   
+
+    <div class="row">
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+                <strong>Name:</strong>
+
+                {{ $vacancy->name }}
+
+            </div>
+
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+                <strong>Vacancy Type:</strong>
+
+                {{ $vacancy->type_vacancy }}
+
+            </div>
+
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+                <strong>Status:</strong>
+
+                {{ $vacancy->status }}
+
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
