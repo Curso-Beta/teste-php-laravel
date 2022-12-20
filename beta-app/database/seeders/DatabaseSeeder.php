741ba53898b50d500f6cb10bfe77b645160c76cb
ramonmendes <dwmom@hotmail.com>
2022-12-20 17:32:54 -0300

    Add new seed to create a vacancy on database

--- beta-app/database/seeders/DatabaseSeeder.php
+++ beta-app/database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CandidatesTableSeeder::class);
+        $this->call(VacanciesTableSeeder::class);
     }
 }
